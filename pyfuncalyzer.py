3482a0aaee74878eb83e97172945d6bbe55eb04c
fractivore <fractivore@protonmail.com>
2022-11-08 03:31:28 -0600

    Increased edge size and encoded edge label as RGB for edge color

--- pyfuncalyzer.py
+++ pyfuncalyzer.py
@@ -32,6 +32,24 @@ imp_dict = dict()
 
 builtins_set = {"abs","all","any","ascii","bin","bool","bytearray","bytes","callable","chr","classmethod","compile","complex","delattr","dict","dir","divmod","enumerate","eval","exec","filter","float","format","frozenset","getattr","globals","hasattr","hash","help","hex","id","input","int","isinstance","issubclass","iter","len","list","locals","map","max","memoryview","min","next","object","oct","open","ord","pow","print","property",""	"","range","repr","reversed","round","set","setattr","slice","sorted","staticmethod","str","sum","super","tuple","type","vars","zip","__import__"}
 
+#TODO: put this in a different module
+def get_hex(value):
+    convert_string = ast.literal_eval(value)
+    convert_hex = hex(convert_string)
+    return convert_hex
+str_to_hex_dict = {'a':5,'b':1,'c':2,'d':3,'e':4,'f':0,'g':6,'h':7,'i':8,'j':6,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':0,'r':1,'s':2,'t':3,'u':4,'v':5,'w':9,'x':7,'y':8,'z':9,' ':10,'_':'11'}
+def encode_text_as_RGB_simple(string_in):
+    string_out_list = ["00","00","00"]
+    index = 0
+    for char in string_in:
+        lst_ind = index%3
+        tmp_int = int(string_out_list[lst_ind],base=16)
+        tmp_int += (int(str_to_hex_dict[char])+tmp_int)%15
+        string_out_list[lst_ind] = str(tmp_int).zfill(2)[0:2]
+        index += 1
+    string_out = "#"+string_out_list[0]+string_out_list[1]+string_out_list[2]
+    return string_out
+
 for node in walked:
     if isinstance(node, ast.Call):
         calls.append(node)
@@ -93,7 +111,8 @@ print(f"imp_dict:{imp_dict}")
 graph_attributes = {
         "bgcolor":"#3D9ABB",
         "decorate":"true",
-        "layout":"osage"
+        "layout":"osage",
+#        "penwidth":"100.0"
 }
 filename_prefix = filename_in.split(".")[0]
 filename_out=f"{filename_prefix}_functions"
@@ -124,10 +143,12 @@ with Diagram(f"{filename_in} Function Diagram", show=False, filename=filename_ou
             for inner in func_dict[mod]['native']:
                 if inner in nodes_by_name['native'] and inner in nodes_by_name['native'][mod]:
                     this_label=f"used by {mod}"
-                    nodes_by_name['native'][inner]['MODNODE'] >> Edge(color="blue", label=this_label) >> nodes_by_name['native'][mod][inner]
+                    nodes_by_name['native'][inner]['MODNODE'] >> Edge(color=encode_text_as_RGB_simple(this_label), label=this_label,penwidth="10") >> nodes_by_name['native'][mod][inner]
+                    print(encode_text_as_RGB_simple(this_label))
             for inner in func_dict[mod]['imports']:
                 for imp in imp_dict:
                     if inner in nodes_by_name['native'][mod] and inner in nodes_by_name['imports'][imp]:
                         this_label=f"used by {mod}"
-                        nodes_by_name['imports'][imp][inner] >> Edge(color="green",label=this_label) >>nodes_by_name['native'][mod][inner]
+                        print(encode_text_as_RGB_simple(this_label))
+                        nodes_by_name['imports'][imp][inner] >> Edge(color=encode_text_as_RGB_simple(this_label),label=this_label,penwidth="10") >>nodes_by_name['native'][mod][inner]
 
